20f7168058aaec4913a058e7579975dae5912708
David Waltermire <david.waltermire@gsa.gov>
2024-10-18 12:30:20 -0400

    Suppressed spotbugs error caused by intentional simple name shadowing.

--- core/src/main/java/gov/nist/secauto/metaschema/core/model/xml/ExternalConstraintsModulePostProcessor.java
+++ core/src/main/java/gov/nist/secauto/metaschema/core/model/xml/ExternalConstraintsModulePostProcessor.java
@@ -6,8 +6,10 @@ import gov.nist.secauto.metaschema.core.model.constraint.IConstraintSet;
 import java.util.Collection;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 @Deprecated(since = "1.2.1", forRemoval = true)
+@SuppressFBWarnings("NM_SAME_SIMPLE_NAME_AS_SUPERCLASS")
 public class ExternalConstraintsModulePostProcessor
     extends gov.nist.secauto.metaschema.core.model.constraint.ExternalConstraintsModulePostProcessor {
 
