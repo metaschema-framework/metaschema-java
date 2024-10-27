f8b19739027d95650cb9dd9b1ddd7add7dbb1057
David Waltermire <david.waltermire@gsa.gov>
2024-10-27 00:19:59 -0400

    Added and fixed some javadocs.

--- core/src/main/java/gov/nist/secauto/metaschema/core/metapath/item/atomic/IIntegerItem.java
+++ core/src/main/java/gov/nist/secauto/metaschema/core/metapath/item/atomic/IIntegerItem.java
@@ -55,8 +55,7 @@ public interface IIntegerItem extends IDecimalItem {
   @NonNull
   static IIntegerItem valueOf(int value) {
     @SuppressWarnings("null")
-    @NonNull
-    BigInteger bigInteger = BigInteger.valueOf(value);
+    @NonNull BigInteger bigInteger = BigInteger.valueOf(value);
     return valueOf(bigInteger);
   }
 
@@ -70,8 +69,7 @@ public interface IIntegerItem extends IDecimalItem {
   @NonNull
   static IIntegerItem valueOf(long value) {
     @SuppressWarnings("null")
-    @NonNull
-    BigInteger bigInteger = BigInteger.valueOf(value);
+    @NonNull BigInteger bigInteger = BigInteger.valueOf(value);
     return valueOf(bigInteger);
   }
 
