3b31753d32212e7a0823ed9ada3c0411f16eb655
A.J. Stein <aj@gsa.gov>
2025-01-03 16:52:13 -0500

    [skip ci] Formatting IDMFlagNodeItem for #323

--- core/src/main/java/gov/nist/secauto/metaschema/core/mdm/IDMFlagNodeItem.java
+++ core/src/main/java/gov/nist/secauto/metaschema/core/mdm/IDMFlagNodeItem.java
@@ -9,7 +9,6 @@ import edu.umd.cs.findbugs.annotations.NonNull;
 import gov.nist.secauto.metaschema.core.mdm.impl.DefinitionFlagNodeItem;
 import gov.nist.secauto.metaschema.core.metapath.StaticContext;
 import gov.nist.secauto.metaschema.core.metapath.item.atomic.IAnyAtomicItem;
-import gov.nist.secauto.metaschema.core.metapath.item.node.IAtomicValuedNodeItem;
 import gov.nist.secauto.metaschema.core.metapath.item.node.IFlagNodeItem;
 import gov.nist.secauto.metaschema.core.model.IFlagDefinition;
 
@@ -17,23 +16,23 @@ import gov.nist.secauto.metaschema.core.model.IFlagDefinition;
  * Represents a Metapath flag node item that is backed by a simple Metaschema
  * module-based data model.
  */
-public interface IDMFlagNodeItem extends IFlagNodeItem, IDMNodeItem, IAtomicValuedNodeItem {
-	  /**
-	   * Create new flag node item that is detached from a parent node item.
-	   *
-	   * @param definition
-	   *          the Metaschema flag definition describing the flag
-	   * @param value
-	   *          the flag's initial value
-	   * @param staticContext
-	   *          the atomic flag value
-	   * @return the new flag node item
-	   */
-	  @NonNull
-	  static IDMFlagNodeItem newInstance(
-	      @NonNull IFlagDefinition definition,
-	      @NonNull IAnyAtomicItem value,
-	      @NonNull StaticContext staticContext) {
-	    return new DefinitionFlagNodeItem(definition, value, staticContext);
-	  }
+public interface IDMFlagNodeItem extends IFlagNodeItem, IDMNodeItem {
+  /**
+   * Create new flag node item that is detached from a parent node item.
+   *
+   * @param definition
+   *          the Metaschema flag definition describing the flag
+   * @param value
+   *          the flag's initial value
+   * @param staticContext
+   *          the atomic flag value
+   * @return the new flag node item
+   */
+  @NonNull
+  static IDMFlagNodeItem newInstance(
+      @NonNull IFlagDefinition definition,
+      @NonNull IAnyAtomicItem value,
+      @NonNull StaticContext staticContext) {
+    return new DefinitionFlagNodeItem(definition, value, staticContext);
+  }
 }
