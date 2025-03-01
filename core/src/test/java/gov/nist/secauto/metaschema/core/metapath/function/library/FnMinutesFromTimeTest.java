348ace5d4805e99e1904d2181ab78dba7229cfd2
A.J. Stein <aj@gsa.gov>
2025-02-28 23:38:27 -0500

    Add minutes-from-time impl and tests for #284

---
+++ core/src/test/java/gov/nist/secauto/metaschema/core/metapath/function/library/FnMinutesFromTimeTest.java
@@ -0,0 +1,38 @@
+/*
+ * SPDX-FileCopyrightText: none
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+package gov.nist.secauto.metaschema.core.metapath.function.library;
+
+import static gov.nist.secauto.metaschema.core.metapath.TestUtils.integer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import gov.nist.secauto.metaschema.core.metapath.ExpressionTestBase;
+import gov.nist.secauto.metaschema.core.metapath.IMetapathExpression;
+import gov.nist.secauto.metaschema.core.metapath.item.atomic.IIntegerItem;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+
+class FnMinutesFromTimeTest
+    extends ExpressionTestBase {
+  private static Stream<Arguments> provideValues() { // NOPMD - false positive
+    return Stream.of(
+        Arguments.of(
+            integer(0),
+            "fn:minutes-from-time(meta:time('13:00:00Z'))"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideValues")
+  void test(@NonNull IIntegerItem expected, @NonNull String metapath) {
+    assertEquals(expected, IMetapathExpression.compile(metapath).evaluateAs(null, IMetapathExpression.ResultType.ITEM,
+        newDynamicContext()));
+  }
+}
