84e55019dd48ca603a27d4921c8f6092a63a2cb3
A.J. Stein <aj@gsa.gov>
2025-02-28 23:43:36 -0500

    Add hours-from-time impl and tests for #284

---
+++ core/src/main/java/gov/nist/secauto/metaschema/core/metapath/function/library/FnHoursFromTime.java
@@ -0,0 +1,74 @@
+/*
+ * SPDX-FileCopyrightText: none
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+package gov.nist.secauto.metaschema.core.metapath.function.library;
+
+import gov.nist.secauto.metaschema.core.metapath.DynamicContext;
+import gov.nist.secauto.metaschema.core.metapath.MetapathConstants;
+import gov.nist.secauto.metaschema.core.metapath.function.FunctionUtils;
+import gov.nist.secauto.metaschema.core.metapath.function.IArgument;
+import gov.nist.secauto.metaschema.core.metapath.function.IFunction;
+import gov.nist.secauto.metaschema.core.metapath.item.IItem;
+import gov.nist.secauto.metaschema.core.metapath.item.ISequence;
+import gov.nist.secauto.metaschema.core.metapath.item.atomic.IDecimalItem;
+import gov.nist.secauto.metaschema.core.metapath.item.atomic.IIntegerItem;
+import gov.nist.secauto.metaschema.core.metapath.item.atomic.ITimeItem;
+
+import java.time.Duration;
+import java.util.List;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+
+/**
+ * Implements the XPath 3.1 <a href=
+ * "https://www.w3.org/TR/xpath-functions-31/#func-hours-from-time">fn:hours-from-time</a>.
+ * function.
+ */
+public final class FnHoursFromTime {
+  private static final String NAME = "hours-from-time";
+  @NonNull
+  static final IFunction SIGNATURE = IFunction.builder()
+      .name(NAME)
+      .namespace(MetapathConstants.NS_METAPATH_FUNCTIONS)
+      .deterministic()
+      .contextIndependent()
+      .focusIndependent()
+      .argument(IArgument.builder()
+          .name("arg")
+          .type(ITimeItem.type())
+          .zeroOrOne()
+          .build())
+      .returnType(IIntegerItem.type())
+      .returnZeroOrOne()
+      .functionHandler(FnHoursFromTime::execute)
+      .build();
+
+  private FnHoursFromTime() {
+    // disable construction
+  }
+
+  @SuppressWarnings("unused")
+  @NonNull
+  private static ISequence<IIntegerItem> execute(@NonNull IFunction function,
+      @NonNull List<ISequence<?>> arguments,
+      @NonNull DynamicContext dynamicContext,
+      IItem focus) {
+    ITimeItem arg = FunctionUtils.asTypeOrNull(arguments.get(0).getFirstItem(true));
+    return arg == null ? ISequence.empty() : ISequence.of(fnMinutesFromTime(arg));
+  }
+
+  /**
+   * Implements <a href=
+   * "https://www.w3.org/TR/xpath-functions-31/#func-hours-from-time">fn:hours-from-time</a>.
+   *
+   * @param arg
+   *          the meta:time item from which to extract the hour component
+   * @return the hour component from the date as integer item
+   */
+  @NonNull
+  public static IIntegerItem fnMinutesFromTime(ITimeItem arg) {
+    return IIntegerItem.valueOf(arg.getHour());
+  }
+}
