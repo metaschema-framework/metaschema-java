6b054ac6f1458bde7864239d8ff45233e0b8506b
A.J. Stein <aj@gsa.gov>
2025-03-04 08:51:44 -0500

    Add fn:function-arity and tests for #275

---
+++ core/src/main/java/gov/nist/secauto/metaschema/core/metapath/function/library/FnFunctionArity.java
@@ -0,0 +1,64 @@
+/*
+ * SPDX-FileCopyrightText: none
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+package gov.nist.secauto.metaschema.core.metapath.function.library;
+
+import gov.nist.secauto.metaschema.core.metapath.DynamicContext;
+import gov.nist.secauto.metaschema.core.metapath.MetapathConstants;
+import gov.nist.secauto.metaschema.core.metapath.StaticMetapathException;
+import gov.nist.secauto.metaschema.core.metapath.function.FunctionUtils;
+import gov.nist.secauto.metaschema.core.metapath.function.IArgument;
+import gov.nist.secauto.metaschema.core.metapath.function.IFunction;
+import gov.nist.secauto.metaschema.core.metapath.item.IItem;
+import gov.nist.secauto.metaschema.core.metapath.item.ISequence;
+import gov.nist.secauto.metaschema.core.metapath.item.atomic.IIntegerItem;
+import gov.nist.secauto.metaschema.core.metapath.item.atomic.IStringItem;
+import gov.nist.secauto.metaschema.core.metapath.type.IItemType;
+import gov.nist.secauto.metaschema.core.util.ObjectUtils;
+
+import java.util.List;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+
+/**
+ * /** Implements <a href=
+ * "https://www.w3.org/TR/xpath-functions-31/#func-function-lookup">fn:function-lookup</a>
+ * functions.
+ */
+public final class FnFunctionArity {
+  @NonNull
+  private static final String NAME = "function-arity";
+
+  @NonNull
+  static final IFunction SIGNATURE = IFunction.builder()
+      .name(NAME)
+      .namespace(MetapathConstants.NS_METAPATH_FUNCTIONS)
+      .deterministic()
+      .contextIndependent()
+      .focusIndependent()
+      .argument(IArgument.builder()
+          .name("func")
+          .type(IFunction.type())
+          .one()
+          .build())
+      .returnType(IIntegerItem.type())
+      .returnZeroOrOne()
+      .functionHandler(FnFunctionArity::execute)
+      .build();
+
+  @SuppressWarnings("unused")
+  @NonNull
+  private static ISequence<IIntegerItem> execute(@NonNull IFunction function,
+      @NonNull List<ISequence<?>> arguments,
+      @NonNull DynamicContext dynamicContext,
+      IItem focus) {
+    IFunction fn = FunctionUtils.asType(ObjectUtils.requireNonNull(arguments.get(0).getFirstItem(true)));
+    return ISequence.of(IIntegerItem.valueOf(fn.arity()));
+  }
+
+  private FnFunctionArity() {
+    // disable construction
+  }
+}
--- core/src/test/java/gov/nist/secauto/metaschema/core/metapath/function/library/FnFunctionArityTest.java
+++ core/src/test/java/gov/nist/secauto/metaschema/core/metapath/function/library/FnFunctionArityTest.java
@@ -6,6 +6,7 @@
 package gov.nist.secauto.metaschema.core.metapath.function.library;
 
 import static gov.nist.secauto.metaschema.core.metapath.TestUtils.integer;
+import static gov.nist.secauto.metaschema.core.metapath.TestUtils.string;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import gov.nist.secauto.metaschema.core.metapath.ExpressionTestBase;
@@ -21,14 +22,17 @@ import java.util.stream.Stream;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
 
-class FnFunctionLookupTest
+class FnFunctionArityTest
     extends ExpressionTestBase {
 
   private static Stream<Arguments> provideValues() { // NOPMD - false positive
     return Stream.of(
         Arguments.of(
             integer(2),
-            "fn:function-arity(fn:substring#2)"));
+            "fn:function-arity(fn:substring#2)"),
+        Arguments.of(
+            integer(1),
+            "fn:function-arity(function($node){name($node)})"));
   }
 
   @ParameterizedTest
